5e82a3a3bd977c1b71cea464b6a96563e113bb34
Anusha Udupa <55906876+udupaAnusha@users.noreply.github.com>
2021-04-28 00:08:54 +0530

    accept the strings which contains all vowels

---
+++ practice/string_vowels.py
@@ -0,0 +1,26 @@
+
+def check(string) :
+
+	string = string.lower()
+	vowels = set("aeiou")
+	s = set({})
+  
+	for char in string :
+		if char in vowels :
+			s.add(char)
+		else:
+			pass
+    
+	if len(s) == len(vowels) :
+		print("Accepted")
+	else :
+		print("Not Accepted")
+
+
+# Driver code
+if __name__ == "__main__" :
+	
+	string = "SEEquoiaL"
+
+	# calling function
+	check(string)
